7db96e6d9214ce7155bb52ecc0ea70291309bcdd
Madlen <madlensarkisian@gmail.com>
2020-08-05 19:30:21 +0300

    [Update] Add login route and auth middleware

--- server/controllers/student.controller.js
+++ server/controllers/student.controller.js
@@ -1,5 +1,6 @@
 const Student = require('../models/Student');
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcrypt');
 
 exports.registerController = (req, res) => {
     const { firstName, lastName, email, password, studentClass } = req.body;
@@ -35,5 +36,38 @@ exports.registerController = (req, res) => {
                                 });
                             })
                       })
+           })
+}
+
+exports.validateController = (req, res) => {
+    const { email, password } = req.body;
+
+    Student.findOne({ email })
+           .then(user => {
+
+               if (!user) {
+                   return res.status(400).json({ msg: 'User does not exist' });
+               }
+            
+               bcrypt.compare(password, user.password)
+                     .then(isMatch => {
+                        if (!isMatch) {
+                            return res.status(400).json({ msg: 'Invalid credentials' });
+                        }
+
+                        jwt.sign(
+                        { id: user._id }, 
+                        process.env.JWT_SECRET,
+                        { expiresIn: 3600 },
+                        (err, token) => {
+                            if (err) {
+                                throw err;
+                            }
+                            res.json({
+                                token
+                            });
+                        })
+                     })
+               
            })
 }

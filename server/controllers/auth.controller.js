be29a00e4c6300e24fe3e021d8f940f23ec69fc5
Madlen <madlensarkisian@gmail.com>
2020-08-05 18:56:19 +0300

    [Update] Added register route and controller

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -1,4 +1,39 @@
+const Student = require('../models/Student');
+const jwt = require('jsonwebtoken');
+
 exports.registerController = (req, res) => {
     const { firstName, lastName, email, password, studentClass } = req.body;
-    console.log(firstName, lastName, email, password, studentClass);
+
+    Student.findOne({ email })
+           .then(user => {
+
+               if (user) {
+                   return res.status(400).json({ msg: 'User already exists' });
+               }
+
+               const newUser = new Student({
+                   firstName,
+                   lastName,
+                   email,
+                   password,
+                   studentClass
+               });
+
+               newUser.save()
+                      .then(user => {
+                          jwt.sign({
+                                id: user._id
+                            }, 
+                            process.env.JWT_SECRET,
+                            { expiresIn: 3600 },
+                            (err, token) => {
+                                if (err) {
+                                    throw err;
+                                }
+                                res.json({
+                                    token
+                                });
+                            })
+                      })
+           })
 }

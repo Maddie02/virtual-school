727b891d34c9d47c5fc1333319905bf83ade5486
Madlen <madlensarkisian@gmail.com>
2020-08-06 17:48:31 +0300

    [New] Added title component

---
+++ client/src/components/Title/Title.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import styles from './Title.module.css';
+
+const Title = ({name}) => {
+    return (
+        <h1 className={styles.title}>{name}</h1>
+    )
+}
+
+export default Title;

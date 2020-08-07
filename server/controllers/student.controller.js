43bd740e24889c3766a29df168b208910c2b941f
Madlen <madlensarkisian@gmail.com>
2020-08-07 22:48:10 +0300

    Jwt no longer can expire

--- server/controllers/student.controller.js
+++ server/controllers/student.controller.js
@@ -26,7 +26,6 @@ exports.registerController = (req, res) => {
                                 id: user._id
                             }, 
                             process.env.JWT_SECRET,
-                            { expiresIn: 3600 },
                             (err, token) => {
                                 if (err) {
                                     throw err;
@@ -59,7 +58,6 @@ exports.validateController = (req, res) => {
                         jwt.sign(
                         { id: user._id }, 
                         process.env.JWT_SECRET,
-                        { expiresIn: 3600 },
                         (err, token) => {
                             if (err) {
                                 throw err;

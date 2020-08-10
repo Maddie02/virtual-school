05024023f26c211fafb1c4168d8ec867d700d4c6
Madlen <madlensarkisian@gmail.com>
2020-08-10 17:43:31 +0300

    [New] Added profile badge to profile

---
+++ client/src/components/Profile/ProfileBadge.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import styles from './ProfileBadge.module.css';
+import { Label } from 'semantic-ui-react';
+
+const ProfileBadge = ({ firstName, lastName, studentClass, posts}) => {
+
+    return (
+        <div className={styles.badge}>
+            <Label as='a' color='red' ribbon>
+                Profile
+            </Label>
+            <div className={styles.name}>{ firstName + ' ' + lastName }</div>
+            <div className={styles.info}>Grade: { studentClass }</div>
+            <div className={styles.info}>Posts: { posts.length }</div>
+        </div>
+    )
+}
+
+export default ProfileBadge;
